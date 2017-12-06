aea2127e521ef9847488d4d522c02bb7238597ec
Harvey.He <Harvey.He@GSSTHARVEY.gsst.com>
2017-12-06 15:47:26 +0800

    update

--- src/main/java/io/cucumber/samples/dw/RunTest.java
+++ src/main/java/io/cucumber/samples/dw/RunTest.java
@@ -4,7 +4,8 @@ import org.junit.runner.RunWith;
 import cucumber.junit.Cucumber; 
 
 @RunWith(Cucumber.class) 
-@Cucumber.Options(format = {"pretty", "html:target/cucumber"}
+//@Cucumber.Options(format = {"pretty", "html:target/cucumber"}
+@Cucumber.Options(format = {"json:target/cucumber.json"}
 , features = {"features/cucumberJava.feature"}
 , glue = {"io.cucumber.samples.dw.steps"}) 
 public class RunTest {
