31e9ac28cae4f5c3cafd9ea2bd105723336ad460
Harvey.He <Harvey.He@GSSTHARVEY.gsst.com>
2017-12-08 15:45:57 +0800

    add Testentity

---
+++ src/main/java/entity/TestEntity.java
@@ -0,0 +1,64 @@
+package entity;
+
+
+
+
+
+
+public class TestEntity {
+	/**
+	 * 是否失败
+	 */
+	private Boolean error;
+	/**
+	 * 返回信息
+	 */
+	private String message;
+	/**
+	 * 返回对象
+	 */
+	private Object result; 
+	
+	
+	public TestEntity() {
+		super();
+	}
+	public TestEntity(Boolean error, String message, Object result) {
+		super();
+		this.error = error;
+		this.message = message;
+		this.result = result;
+	}
+	public Boolean getError() {
+		return error;
+	}
+	public void setError(Boolean error) {
+		this.error = error;
+	}
+	public String getMessage() {
+		return message;
+	}
+	public void setMessage(String message) {
+		this.message = message;
+	}
+	public Object getResult() {
+		return result;
+	}
+	public void setResult(Object result) {
+		this.result = result;
+	}
+	
+	public static TestEntity getReturn(Boolean error,String message,Object obj) {
+		TestEntity result = new TestEntity();
+		result.setError(error);
+		result.setMessage(message);
+		result.setResult(obj);
+		return result;
+	} 
+	public void setReturn(Boolean error,String message,Object result){
+		this.result=result;
+		this.error=error;
+		this.message=message;
+	}
+}
+
